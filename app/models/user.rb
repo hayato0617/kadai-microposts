00bbc880dbd083d6eac05b61e0272968ee1124e5
hayato0617 <against_disasters@outlook.jp>
2020-10-25 05:44:29 +0000

    post

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
 has_secure_password
+has_many :microposts
 end
